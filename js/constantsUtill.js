6aeb9baefebeddaccff04c87be1cc306a1ebb41a
tommasoaldinucci <tommaso.aldinucci@icloud.com>
2018-09-17 12:46:36 +0200

    Latest version

--- js/constantsUtill.js
+++ js/constantsUtill.js
@@ -1,15 +1,23 @@
-var scaled_G = 6.67408*Math.pow(10,-23);
+var scaled_G = 6.67408*Math.pow(10,-11);
 
+//SUN
 const SUN_PARTICLES = 3 * 100000;
 const SUN_INIT_POSITION = new THREE.Vector3(0.0, 0.0, 0.0);
 const SUN_INIT_VELOCITY = new THREE.Vector3(0.0, 0.0, 0.0);
 const SUN_INIT_ACCELERATION = new THREE.Vector3(0.0, 0.0, 0.0);
-const SUN_RADIUS = 10;
+const SUN_RADIUS = 20;
 const SUN_MASS = 5.989 * Math.pow(10, 30);
 
+//EARTH
+var EARTH_VELOCITY = new THREE.Vector3(-1.0, 0.0, 0.0);
+var EARTH_INIT_POSITION = new THREE.Vector3(0.0, 0.0, -150.0);
+EARTH_VELOCITY = vectorScalarProduct(EARTH_VELOCITY, Math.sqrt(scaled_G*SUN_MASS/(vectorNorm(EARTH_INIT_POSITION)*1000)));
+
+
 const ASTEROID_PARTICLES = 3 * 100000;
-const ASTEROID_INIT_POSITION = new THREE.Vector3(228.0, 0.0, 0.0);
-const ASTEROID_INIT_VELOCITY = new THREE.Vector3(0.0, 0.0, 0.0);
+const ASTEROID_INIT_POSITION = new THREE.Vector3(0.0, -150.0, 0.0);
+//const ASTEROID_INIT_VELOCITY = new THREE.Vector3(0.0, 0.0, 0.0);
 const ASTEROID_INIT_ACCELERATION = new THREE.Vector3(0.0, 0.0, 0.0);
 const ASTEROID_RADIUS = 4;
 const ASTEROID_MASS = 5.9 * Math.pow(10, 24);
+const ASTEROID_INIT_VELOCITY = vectorScalarProduct(new THREE.Vector3(1, 0, 0), Math.sqrt(scaled_G*SUN_MASS/(vectorNorm(ASTEROID_INIT_POSITION)*1000)));
--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,13 @@
+
+
 function createPointsPlanet(radius, numPoints, initPosition, material) {
     var geometry = new THREE.BufferGeometry();
 
     var positions = new Float32Array(numPoints);
 
     for (var i = 0; i < numPoints; i+=3) {
-        var rho = Math.random() * radius;
+        //var rho = Math.pow(Math.random() * radius, 0.25);
+        var rho = Math.pow(Math.random(), 0.25) * radius;
         var theta = Math.random() * 360;
         var phi = 90 - (Math.random() * 180);
 
@@ -53,18 +56,6 @@ function vectorNorm(v){
     return Math.sqrt(new THREE.Vector3().copy(v).dot(v));
 }
 
-function movePlanet(planet, dx, dy, dz){
-    for (var i = 0; i < planet.obj.geometry.attributes.position.array.length; i += 3) {
-        planet.obj.geometry.attributes.position.array[i] += dx;
-        planet.obj.geometry.attributes.position.array[i+1] += dy;
-        planet.obj.geometry.attributes.position.array[i+2] += dz;
-    }
-    planet.obj.geometry.attributes.position.needsUpdate = true;
-
-    planet.obj.geometry.computeBoundingSphere();
-
-    return planet.obj.geometry.boundingSphere.center;
-}
 
 function movePlanet(planet, dx, dy, dz, initPosition){
     for (var i = 0; i < planet.obj.geometry.attributes.position.array.length; i += 3) {
@@ -81,9 +72,10 @@ function movePlanet(planet, dx, dy, dz, initPosition){
 
 function newton_movement(planet1, planet2, initPositionPlanet1){
 
-    var delta_t = 0.001;
+    var delta_t = 0.00001;
 
     var r = vectorSubtraction(planet2.center, planet1.center);
+    r = vectorScalarProduct(r, 1000);
 
     var a = vectorScalarProduct(r, 1/vectorNorm(r))
     a = vectorScalarProduct(a, scaled_G*planet2.mass/(vectorNorm(r)*vectorNorm(r)));
@@ -92,7 +84,7 @@ function newton_movement(planet1, planet2, initPositionPlanet1){
 
     var delta_s = vectorSum(vectorScalarProduct(delta_v, delta_t), vectorScalarProduct(a, 0.5*delta_t*delta_t));
 
-    planet1.center = movePlanet(planet1, delta_s.x, delta_s.y, delta_s.z, initPositionPlanet1);
+    planet1.center = movePlanet(planet1, delta_s.x/1000, delta_s.y/1000, delta_s.z/1000, initPositionPlanet1);
     planet1.velocity = delta_v;
     planet1.acceleration = a;
 }
@@ -104,4 +96,36 @@ function anelastic_impact(planet1, planet2){
     E_k_lost = (planet1.mass*planet2.mass)*Math.pow(vectorNorm(new THREE.Vector3().subVectors(planet2.velocity,planet1.velocity)), 2)/(2*(planet1.mass+planet2.mass));
 
     return [v_cm_f, E_k_lost];
+}
+
+function shake(planet){
+
+    //SHAKE PARTICLES
+    for (var i=0; i<planet.obj.geometry.attributes.position.array.length; i+=3)
+    {
+        planet.obj.geometry.attributes.position.array[i] += (1 - 2 * Math.random()) * planet.obj.geometry.attributes.position.array[i] / 1000;
+        planet.obj.geometry.attributes.position.array[i + 1] += (1 - 2 * Math.random()) * planet.obj.geometry.attributes.position.array[i + 1] / 1000;
+        planet.obj.geometry.attributes.position.array[i + 2] += (1 - 2 * Math.random()) * planet.obj.geometry.attributes.position.array[i + 2] / 1000;
+    }
+    planet.obj.geometry.attributes.position.needsUpdate = true;
+
+
+}
+function planet_newton_movement(planet1, velocity, planet2){
+
+    var delta_t = 0.00001;
+
+    var r = vectorSubtraction(planet2.center, planet1.position);
+    r = vectorScalarProduct(r, 1000);
+
+    var a = vectorScalarProduct(r, 1/vectorNorm(r))
+    a = vectorScalarProduct(a, scaled_G*planet2.mass/(vectorNorm(r)*vectorNorm(r)));
+
+    var delta_v = vectorSum(vectorScalarProduct(a, delta_t), velocity);
+
+    var delta_s = vectorSum(vectorScalarProduct(delta_v, delta_t), vectorScalarProduct(a, 0.5*delta_t*delta_t));
+
+    planet1.position.set(planet1.position.x+delta_s.x/1000, planet1.position.y+delta_s.y/1000, planet1.position.z+delta_s.z/1000);
+
+    return delta_v;
 }
