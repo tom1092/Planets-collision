39a14a02277ca8bbf3fc0a2c9b9282a2e7789b5d
tommasoaldinucci <tommaso.aldinucci@icloud.com>
2018-09-18 17:53:00 +0200

    Latest version

--- js/constantsUtill.js
+++ js/constantsUtill.js
@@ -4,7 +4,7 @@
 var scaled_G = 6.67408*Math.pow(10,-11);
 
 //SUN
-const SUN_PARTICLES = 3 * 1000;
+const SUN_PARTICLES = 3 * 15000;
 const SUN_INIT_POSITION = new THREE.Vector3(0.0, 0.0, 0.0);
 const SUN_INIT_VELOCITY = new THREE.Vector3(0.0, 0.0, 0.0);
 const SUN_INIT_ACCELERATION = new THREE.Vector3(0.0, 0.0, 0.0);
@@ -16,8 +16,14 @@ var EARTH_VELOCITY = new THREE.Vector3(-1.0, 0.0, 0.0);
 var EARTH_INIT_POSITION = new THREE.Vector3(0.0, 0.0, -150.0);
 EARTH_VELOCITY = vectorScalarProduct(EARTH_VELOCITY, Math.sqrt(scaled_G*SUN_MASS/(vectorNorm(EARTH_INIT_POSITION)*1000)));
 
+//JUPITER
+var JUPITER_VELOCITY = new THREE.Vector3(449.0, 449.0, 449.0).cross(new THREE.Vector3(449.0, 0.0, 449.0));
+JUPITER_VELOCITY = vectorScalarProduct(JUPITER_VELOCITY, 1.0/vectorNorm(JUPITER_VELOCITY));
+var JUPITER_INIT_POSITION = new THREE.Vector3(449.0, 449.0, 449.0);
+JUPITER_VELOCITY = vectorScalarProduct(JUPITER_VELOCITY, Math.sqrt(scaled_G*SUN_MASS/(vectorNorm(JUPITER_INIT_POSITION)*1000)));
 
-const ASTEROID_PARTICLES = 3 * 1000;
+
+const ASTEROID_PARTICLES = 3 * 5000;
 const ASTEROID_INIT_POSITION = new THREE.Vector3(0.0, -150.0, 0.0);
 //const ASTEROID_INIT_VELOCITY = new THREE.Vector3(1000.0, 0.0, 0.0);
 const ASTEROID_INIT_ACCELERATION = new THREE.Vector3(0.0, 0.0, 0.0);
